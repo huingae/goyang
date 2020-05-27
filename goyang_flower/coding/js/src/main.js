084d90530e001eda3f1bfb112b257dae937589cb
sangah <iow45675@gmail.com>
2020-05-27 18:02:06 +0900

    0527

--- goyang_flower/coding/js/src/main.js
+++ goyang_flower/coding/js/src/main.js
@@ -1,1 +1,26 @@
 // main.js
+(function($){
+
+	var win =$(window);
+	var winH = win.height();
+
+	var addBannerBox = $('#adBannerBox');
+	var storyBox = $('#storyBox');
+	var videoBox = $("#videoBox");
+	addBannerBox.height(winH);
+	storyBox.height(winH);
+	videoBox.height(winH);
+
+	win.resize(function(){
+		addBannerBox.height(winH);
+		storyBox.height(winH);
+		videoBox.height(winH);
+	});
+
+	// scrollIcon-----------------------------
+	var scrollIcon = $('.scroll_icon');
+	function loopBoat(){scrollIcon.animate({bottom:'2%'},500).animate({bottom:0},500,loopBoat);}
+	loopBoat();
+	// ===============================================
+
+})(jQuery);
